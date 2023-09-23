c65644190babe4e88231725665328adeb5ef78f8
ibrahimhaji0708 <98814268+ibrahimhaji0708@users.noreply.github.com>
2023-09-23 21:19:44 +0530

    bloc providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blue_dog/bloc/email_pass_input_bloc.dart';
 import 'package:blue_dog/forgot_password.dart';
 import 'package:blue_dog/email_password_input.dart';
 import 'package:blue_dog/register.dart';
@@ -8,7 +9,9 @@ import 'package:supabase/supabase.dart';
 import 'bloc/main_bloc.dart';
 
 void main() {
-  runApp(const BlueDog());
+  runApp(
+    const BlueDog(),
+  );
 }
 
 final supabase = SupabaseClient(
@@ -22,7 +25,7 @@ class BlueDog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => MainBloc(),
+      create: (context) => MainBloc(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Blue Dog',
@@ -45,86 +48,90 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final mainBloc = BlocProvider.of<MainBloc>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BLUE DOG'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 63.0),
-              Image.asset(
-                'assets/images/blue_dog.png',
-                height: 150.0,
-                width: 150.0,
-              ),
-              const SizedBox(height: 70.0),
-              EmailPasswordInput(
-                controller: TextEditingController(text: mainBloc.state.email),
-                hintText: 'Email',
-                onValidationChanged: (isValid) {
-                  mainBloc.add(EmailChanged(mainBloc.state.email));
-                },
-              ),
-              EmailPasswordInput(
-                controller: TextEditingController(text: mainBloc.state.password),
-                hintText: 'Password',
-                isPassword: true,
-                onValidationChanged: (isValid) {
-                  mainBloc.add(PasswordChanged(mainBloc.state.password));
-                },
-              ),
-              const SizedBox(height: 20),
-              const SizedBox(height: 10.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const ForgotPassScreen(),
-                    ),
-                  );
-                },
-                child: const Text('FORGOT PASSWORD'),
-              ),
-              const SizedBox(height: 20.0),
-              // ignore: sized_box_for_whitespace
-              Container(
-                width: 350,
-                height: 50,
-                child: OutlinedButton(
-                  onPressed: () {
-                    // Dispatch a login event to MainBloc
-                    mainBloc.add(const LoginPage() as MainEvent);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    textStyle: MaterialStateTextStyle.resolveWith(
-                      (states) => const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  child: const Text('Login'),
+    return BlocProvider(
+      create: (context) => EmailPasswordInputBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('BLUE DOG'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 63.0),
+                Image.asset(
+                  'assets/images/blue_dog.png',
+                  height: 150.0,
+                  width: 150.0,
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              // ignore: sized_box_for_whitespace
-              Container(
-                width: 350,
-                height: 50,
-                child: OutlinedButton(
+                const SizedBox(height: 70.0),
+                EmailPasswordInput(
+                  controller: TextEditingController(text: mainBloc.state.email),
+                  hintText: 'Email',
+                  onValidationChanged: (isValid) {
+                    mainBloc.add(EmailChanged(mainBloc.state.email));
+                  },
+                ),
+                EmailPasswordInput(
+                  controller:
+                      TextEditingController(text: mainBloc.state.password),
+                  hintText: 'Password',
+                  isPassword: true,
+                  onValidationChanged: (isValid) {
+                    mainBloc.add(PasswordChanged(mainBloc.state.password));
+                  },
+                ),
+                const SizedBox(height: 20),
+                const SizedBox(height: 10.0),
+                TextButton(
                   onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
-                        builder: (context) => const RegisterScreen(),
+                        builder: (context) => const ForgotPassScreen(),
                       ),
                     );
                   },
-                  child: const Text('Register'),
+                  child: const Text('FORGOT PASSWORD'),
                 ),
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width: 350,
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      // Dispatch a login event to MainBloc
+                      mainBloc.add(const LoginPage() as MainEvent);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      textStyle: MaterialStateTextStyle.resolveWith(
+                        (states) => const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    child: const Text('Login'),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width: 350,
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text('Register'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
