5627854fe89af0783b75f4c4185d530cab000e2b
mocodesmor <98814268+ibrahimhaji0708@users.noreply.github.com>
2023-10-18 11:15:00 +0530

    checkLogin

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:blue_dog/main.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  void _logOut(BuildContext context) {
+  void _logOut(BuildContext context) async {
+    // final prefs = await SharedPreferences.getInstance();
+    // await prefs.remove('user_token');
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    authProvider.logout();
+
+    // Navigate back to the login page
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (context) => const LoginPage(),
@@ -14,33 +22,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('home page'),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
+    final authProvider = Provider.of<AuthProvider>(context);
+
+    return PopScope(
+      canPop: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('home page'),
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ),
+          actions: [
+            if (authProvider.isLoggedIn)
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () {
+                _logOut(context);
+              },
+            ),
+          ],
+        ),
+        drawer: const Drawer(
+          child: Center(
+            child: Text('hello there', textAlign: TextAlign.center),
           ),
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              _logOut(context);
-            },
-          ),
-        ],
-      ),
-      drawer: const Drawer(
-        child: Center(
-          child: Text('hello there', textAlign: TextAlign.center),
+        body: Container(
+          // Add your home screen content here
         ),
-      ),
-      body: Container(
-        // Add your home screen content here
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,31 @@ import 'package:blue_dog/register.dart';
 import 'package:blue_dog/verification.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase/supabase.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
-  runApp(const BlueDog());
+  runApp(ChangeNotifierProvider(
+    child: const BlueDog(),
+    create: (context) => AuthProvider(),
+  ));
+  //checkUserSession();
 }
+
+// Future<void> saveUserSession(String token) async {
+//   final prefs = await SharedPreferences.getInstance();
+//   await prefs.setString('user_token', token);
+// }
+
+// Future<void> checkUserSession() async {
+//   final prefs = await SharedPreferences.getInstance();
+//   final token = prefs.getString('user_token');
+
+//   if (token != null) {
+//     await _login(token);
+//   }
+// }
 
 final supabase = SupabaseClient(
   'https://ydvzfbbrjpyccxoabdxz.supabase.co',
@@ -114,6 +135,22 @@ class BlueDog extends StatelessWidget {
       ),
       home: const LoginPage(),
     );
+  }
+}
+
+class AuthProvider with ChangeNotifier {
+  bool _isLoggedIn = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+
+  void login() {
+    _isLoggedIn = true;
+    notifyListeners();
+  }
+
+  void logout() {
+    _isLoggedIn = false;
+    notifyListeners();
   }
 }
 
